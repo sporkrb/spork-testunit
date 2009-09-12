30e1d762564a57633c84a809a88f3cb5d40adc9d
Tim Harper <timcharper@gmail.com>
2009-09-12 03:30:08 -0400

    force the STDOUT constant to get the TestUnit runner to output to the desired stream

--- lib/spork/test_framework/test_unit.rb
+++ lib/spork/test_framework/test_unit.rb
@@ -3,6 +3,7 @@ class Spork::TestFramework::TestUnit < Spork::TestFramework
   HELPER_FILE = File.join(Dir.pwd, "test/test_helper.rb")
 
   def run_tests(argv, stderr, stdout)
+    Object.send(:remove_const, :STDOUT); Object.send(:const_set, :STDOUT, stdout)
     require 'test/unit/autorunner'
     r = Test::Unit::AutoRunner.new(true)
     exit(false) unless r.process_args(argv)
